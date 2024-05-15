12bff9af34188e70919e47e10110445dc8b26c94
arianisepti <arianidsepti01@gmail.com>
2024-05-15 10:29:39 +0700

    all changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -33,7 +34,20 @@ class HomeController extends Controller
         [
             'email.required'=>'Email wajib diisi',
             'password.required'=>'Password wajib diisi',
-        ],
+        ]
     );
+
+    $infologin = [
+        'email'=> $request->email,
+        'password'=> $request->password,
+
+
+    ];
+
+    if(Auth::attempt($infologin)){
+        echo "sukses"; exit();
+    }else{
+        return redirect('/login')->withErrors('Username dan Password yang dimasukkan tidak sesuai')->withInput();
+    }
     }
 }
