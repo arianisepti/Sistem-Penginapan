ea4ec9d4a35666bbe8f418051a35f3324ddae552
arianisepti <arianidsepti01@gmail.com>
2024-05-15 09:47:56 +0700

    all changes

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -28,21 +28,22 @@
 
     <!-------------------- ------ Right Box ---------------------------->
 
-        @if($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $item)
-                    <li>{{ $item }}</li>
-                @endforeach
-            </ul>
-        </div>
-        @endif
+        
        <form class="col-md-6 right-box" id="myForm" action="" method="post">
         @csrf
           <div class="row align-items-center">
                 <div class="header-text mb-4">
                      <p>Enjoy All Our Facility</p>
                 </div>
+                @if($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $item)
+                            <li>{{ $item }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
                 <div class="input-group mb-3">
                     <input type="email" value="{{ old('email') }}" class="form-control form-control-lg bg-light fs-6" id="email" name="email" placeholder="Email">
                 </div>
