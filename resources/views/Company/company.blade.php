e0d07e9281f4b2f18d8c6f24341629b7368f2837
IhfanNurMaulana <ihfannurmaulana.com>
2024-06-10 09:45:58 +0700

    all changes

--- resources/views/Company/company.blade.php
+++ resources/views/Company/company.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8" />
-    <title>Foursen Company</title>
+    <title>Foursen Network Company</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
     <meta content="" name="keywords" />
     <meta content="" name="description" />
